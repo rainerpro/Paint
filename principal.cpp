f52d6d44cdbb43394132cf58c153e7ac93e26a37
“ <rainernazareno13faztweb.com>
2022-02-06 15:48:43 -0500

    IMPLEMENTACION DE PROCESOS

--- principal.cpp
+++ principal.cpp
@@ -6,10 +6,164 @@ principal::principal(QWidget *parent)
     , ui(new Ui::principal)
 {
     ui->setupUi(this);
+    //Instancia de la imagen
+    m_imagen = new QImage(this->size(), QImage::Format_ARGB32_Premultiplied);
+
+    //Rellenar de blanco
+    m_imagen->fill(Qt::white);
+
+    //Instanciar el objeto painter
+    m_painter = new QPainter(m_imagen);
+    m_painter->setRenderHint(QPainter::Antialiasing);
+    m_color = Qt::black;
+    m_width = DEFAULT_ANCHO;
+    m_opcion = 1;
 }
 
 principal::~principal()
 {
     delete ui;
 }
+
+void principal::paintEvent(QPaintEvent *event)
+{
+    //Crear otro painter (local)
+    QPainter painter(this);
+
+    //Dibujar la imagen
+    painter.drawImage(0,0, *m_imagen);
+
+    //Aceptar el evento
+    event->accept();
+}
+
+void principal::mousePressEvent(QMouseEvent *event)
+{
+    if(m_opcion == 1){
+        m_ptInicial = event->pos();
+    }else if (m_opcion == 2){
+        if (m_toogle){
+            m_ptInicial = event->pos();
+        }else {
+            m_ptFinal = event->pos();
+            QPen pincel;
+            pincel.setColor(m_color);
+            pincel.setWidth(m_width);
+            m_painter->setPen(pincel);
+            m_painter->drawLine(m_ptInicial, m_ptFinal);
+            //Redibujar la interfaz grafica
+            update();
+        }
+    }else if(m_opcion == 3){
+        if (m_toogle){
+            m_ptInicial = event->pos();
+        }else {
+            m_ptFinal = event->pos();
+            //Dibujar rectangulo
+            int ancho = m_ptFinal.x() - m_ptInicial.y();
+            int alto = m_ptFinal.y() - m_ptInicial.y();
+
+            QPen pincel;
+            pincel.setColor(m_color);
+            pincel.setWidth(m_width);
+            m_painter->setPen(pincel);
+            m_painter->drawRect(m_ptInicial.x(), m_ptInicial.y(), ancho, alto);
+            //Redibujar la interfaz grafica
+            update();
+        }
+    }else if(m_opcion == 4){
+        if (m_toogle){
+            m_ptInicial = event->pos();
+        }else {
+            m_ptFinal = event->pos();
+            //Dibujar rectangulo
+            QPen pincel;
+            pincel.setColor(m_color);
+            pincel.setWidth(m_width);
+            QRectF circulo(m_ptInicial, m_ptFinal);
+            m_painter->setPen(pincel);
+            m_painter->drawEllipse(circulo);
+            //Redibujar la interfaz grafica
+            update();
+        }
+    }
+    m_toogle = !m_toogle;
+}
+
+void principal::mouseMoveEvent(QMouseEvent *event)
+{
+    if(m_opcion == 1){
+        m_ptFinal = event->pos();
+        QPen pincel;
+        pincel.setColor(m_color);
+        pincel.setWidth(m_width);
+        m_painter->setPen(pincel);
+        m_painter->drawLine(m_ptInicial, m_ptFinal);
+    }
+    update();
+    m_ptInicial = m_ptFinal;
+}
+
+void principal::on_actionCOLOR_triggered()
+{
+    m_color = QColorDialog::getColor(m_color,this,"Color del pincel");
+}
+
+void principal::on_actionANCHO_triggered()
+{
+    m_width = QInputDialog::getInt(this, "Ancho del pincel", "Inserte el ancho del pincel",1,1,10);
+}
+
+void principal::on_actionGUARDAR_triggered()
+{
+    //Crear un objeto QDir a partir del directorio del usuario
+    QDir directorio = QDir::home();
+
+    //Agregar al path absoluto del objeto un nombre por defecto del archivo
+    QString pathArchivo = directorio.absolutePath();
+
+    QString nombreArchivo = QFileDialog::getSaveFileName(
+                this,"Guardar imagen",pathArchivo,"Imagenes (*.png)");
+    if (!nombreArchivo.isEmpty()){
+        if (m_imagen->save(nombreArchivo))
+            QMessageBox::information(this,"Guardar imagen","Archivo guarado en: " + nombreArchivo);
+        else
+            QMessageBox::warning(this,"Guardar imagen","No se pudo guardar el archivo");
+    }
+}
+
+void principal::on_actionLIBRE_triggered()
+{
+    m_opcion = 1;
+}
+
+void principal::on_actionLINEAS_triggered()
+{
+    m_opcion = 2;
+    m_toogle = true;
+}
+
+void principal::on_actionRECTANGULOS_triggered()
+{
+    m_opcion = 3;
+}
+
+void principal::on_actionCIRCUFERENCIA_triggered()
+{
+    m_opcion = 4;
+}
+
+void principal::on_actionNUEVO_triggered()
+{
+    m_imagen->fill(Qt::white);
+    m_color = Qt::black;
+    m_opcion = 1;
+    update();
+}
+
+void principal::on_actionSALIR_triggered()
+{
+    this->close();
+}
+
 
--- principal.h
+++ principal.h
@@ -2,6 +2,17 @@
 #define PRINCIPAL_H
 
 #include <QMainWindow>
+#include <QMouseEvent>
+#include <QImage>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QColorDialog>
+#include <QInputDialog>
+#include <QDir>
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#define DEFAULT_ANCHO 2;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class principal; }
@@ -15,7 +26,40 @@ public:
     principal(QWidget *parent = nullptr);
     ~principal();
 
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+
+private slots:
+    void on_actionCOLOR_triggered();
+
+    void on_actionANCHO_triggered();
+
+    void on_actionGUARDAR_triggered();
+
+    void on_actionLIBRE_triggered();
+
+    void on_actionLINEAS_triggered();
+
+    void on_actionRECTANGULOS_triggered();
+
+    void on_actionCIRCUFERENCIA_triggered();
+
+    void on_actionNUEVO_triggered();
+
+    void on_actionSALIR_triggered();
+
 private:
     Ui::principal *ui;
+    QPen pincel;
+    QColor m_color; //Color de la linea
+    int m_width; //Ancho de la linea
+    int m_opcion; //Opcion de dibujo
+    QImage *m_imagen; //Imagen sobre la cual se va a dibujar
+    QPainter *m_painter; //El objeto painter
+    QPoint m_ptInicial; //Punto inicial para dibujar
+    QPoint m_ptFinal;   //Punto final para dibujar
+    bool m_toogle = true;
 };
 #endif // PRINCIPAL_H
